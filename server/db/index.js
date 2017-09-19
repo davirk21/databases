7d02914d72b7f73cc7b07987884e10a728d5dea3
davirk21 <davirk21@gmail.com>
2017-09-18 21:27:12 -0700

    created table

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,21 @@
 var mysql = require('mysql');
 
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'me',
+  password : 'plantlife',
+  database : 'chat'
+});
+ 
+connection.connect();
+ 
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+ 
+connection.end();
+
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
--- server/schema.sql
+++ server/schema.sql
@@ -1,10 +1,31 @@
+DROP DATABASE IF EXISTS chat;
 CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE user (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+  userName CHAR(25) 
+  
+);
+
+CREATE TABLE room (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+  roomName CHAR(25) 
+);
+
 CREATE TABLE messages (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+  message CHAR(200) , 
+  user INT,
+  room INT,
+  
+  FOREIGN KEY (user) REFERENCES user(id),
+  FOREIGN KEY (room) REFERENCES room(id)
   /* Describe your table here.*/
 );
+
+
 
 /* Create other tables and define schemas for them here! */
 
